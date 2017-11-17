740ff4f5ef7234b89a90e0aca468817f936ffb3e
KITRI <KITRI@KITRI-PC>
2017-11-17 13:44:27 +0900

    gogogogo

---
+++ src/controller/controllgit.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class controllgit {
+	
+	public void register() {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Connection conn = null;
+		Statement stmt = null;
+		
+		
+		
+		
+		
+	}
+
+}
